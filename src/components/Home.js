81a82c31c12f13d176a21bfafea3e5ad41f7997f
TsikaBullRat <tsika1996190514@gmail.com>
2021-06-23 11:18:17 +0200

    Tester

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,14 @@
+import Header from './Header'
 
+function Home(){
+
+    return(
+        <div className="home">
+            <Header/>
+            <div className="banner_text">
+                <h2>Hi this is meant for:</h2>
+                <p>Anything the owner of this website wishes to say</p>
+            </div>
+        </div>
+    );
+}
