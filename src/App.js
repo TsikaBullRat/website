b6fa69dbb5a9ea27c7d2e5fc07077e0df941f28a
TsikaBullRat <tsika1996190514@gmail.com>
2021-06-25 13:02:41 +0200

    Last frame updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
+import logo  from './logo.svg'
 import Home from './components/Home';
+import Header from './components/Header';
 import Aboutus from './components/Aboutus';
 import Ourimpacts from './components/Ourimpacts';
 import Contacts from './components/Contacts';
 import Full_Screen_Slide from './components/Full_Sreen_Slide';
+import Hero from './components/hero';
 
 
 function App() {
   return (
-    <div>
-      <Aboutus/>
-
-      {/*<Home/>*/}
+    <div className="container-fluid">
+      {/*<Header/>*/}
+      <Home/>
       {/*<Ourimpacts/>*/}
       {/*<Aboutus/>*/}
       {/*<Full_Screen_Slide/>*/}
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,26 +3,20 @@
 function Header() {
 
     return (
-        <div className="header navbar navbar-expand-lg">
-            <h2>Lifeline</h2>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#Content">
-                <span className="nav-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+            <a className="navbar-brand" href="#">Lifeline</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="links" id="Content">
-                <span className="nav-item">
-                    <a className="nav-link">Home</a>
-                </span>
-                <span className="nav-item">
-                    <a nav-link>About Us</a>
-                </span>
-                <span className="nav-item">
-                    <a nav-link>Our Impacts</a>
-                </span>
-                <span className="nav-item">
-                    <a nav-link>Conatcts</a>
-                </span>
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <a className="nav-link" href="#">Home</a>
+                    <a className="nav-link" href="#">About Us</a>
+                    <a className="nav-link" href="#">Our Impacts</a>
+                    <a className="nav-link" href="#">Contacts</a>
+                </div>
             </div>
-        </div>
+        </nav>
     );
 }
 
