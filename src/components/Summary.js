57fb2c43afda9a81d3e74f1370408222316ddc62
TsikaBullRat <tsika1996190514@gmail.com>
2021-06-29 16:53:12 +0200

    Last Minute Changes

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -6,9 +6,9 @@ function Summary() {
         <div className="container-fluid">
             <div className="row mb-4 ml-5 mr-5">
 
-                <div className="col-sm-12 col-lg-6 text-center mb-5">
+                <div className="col-sm-12 col-lg-4 text-center mb-5">
                     <h3>What we're about</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet mauris commodo quis imperdiet massa tincidunt nunc. Vulputate odio ut enim blandit volutpat. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit. Sapien eget mi proin sed libero enim sed. Eget nulla facilisi etiam dignissim diam. Tincidunt id aliquet risus feugiat in ante metus dictum at. Tincidunt id aliquet risus feugiat. Amet nulla facilisi morbi tempus iaculis urna. Enim diam vulputate ut pharetra sit. Commodo elit at imperdiet dui accumsan sit amet nulla facilisi. Pretium aenean pharetra magna ac. Viverra maecenas accumsan lacus vel. Congue quisque egestas diam in arcu. Interdum velit euismod in pellentesque massa placerat duis. At in tellus integer feugiat scelerisque. Augue mauris augue neque gravida in fermentum.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ....</p>
                     <Link exact to="/about">
                         <button className="btn btn-primary">Want to know more</button>
                     </Link>
