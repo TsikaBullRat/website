12949cabbfb089e79b3bc24514fb09fbe3203ffa
TsikaBullRat <tsika1996190514@gmail.com>
2021-07-13 12:32:10 +0200

    Freaking Github

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,17 +22,9 @@ export const Home = () => {
 
       {
         loading ? (
-<<<<<<< HEAD
-<<<<<<< HEAD
           <div className="text-center" style={{ marginTop: 300 }}>
             <Loader size={300} color={"#fa8701"} loading={loading} />
           </div>
-=======
-          <div className="text-center" style={{ marginTop: 300 }}><Loader size={300} color={"#27394a"} loading={loading} /></div>
->>>>>>> parent of 89f4158... final commit
-=======
-          <div className="text-center" style={{ marginTop: 300 }}><Loader size={300} color={"#fa8701"} loading={loading} /></div>
->>>>>>> parent of e15dd13... Form function
         ) : (
         <>  
           <Jumbotron/>
